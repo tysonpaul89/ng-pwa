9e3a6a9844af4107e277ff6db9a56aa60cd507ef
Tyson Paul <tyson.paul@digitalmesh.com>
2018-07-02 20:11:29 +0530

    new buttons added

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+  isPushNotifyActive = true;
   constructor() { }
 
   ngOnInit() {
+  }
+
+  sendPushNotification() {
+
+  }
+
+  toggleNotificationSubscription(pushSubStatus) {
+    this.isPushNotifyActive = pushSubStatus;
   }
 
 }
