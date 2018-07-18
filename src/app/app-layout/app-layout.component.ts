6a9946927fdcb44379f997e839e061ea370cd84b
Tyson Paul <tyson.paul@digitalmesh.com>
2018-07-18 19:20:42 +0530

    nav button foucs on close issue fixed

--- src/app/app-layout/app-layout.component.ts
+++ src/app/app-layout/app-layout.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import {MatSidenav} from '@angular/material/sidenav';
+import {MatButton} from '@angular/material/button';
 
 @Component({
   selector: 'app-layout',
@@ -10,6 +12,7 @@ import { map } from 'rxjs/operators';
 })
 export class AppLayoutComponent {
 
+  @ViewChild(MatButton) menuButton: MatButton;
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
       map(result => result.matches)
@@ -17,4 +20,18 @@ export class AppLayoutComponent {
 
   constructor(private breakpointObserver: BreakpointObserver) {}
 
+  dissmissNav(drawer: MatSidenav, menuButton: MatButton) {
+    // Don't hide side nav on desktops
+    if (drawer.mode != 'side') {
+      drawer.toggle();
+      (<any>menuButton)._focusMonitor.stopMonitoring(menuButton._getHostElement());
+    }
   }
+
+  // handleClose = (button: MatButton) => {
+  //   console.log(button);
+  //   // <any> needed to satisfy ts compiler, otherwise it won't allow accessing MatButton's private methods
+  //   (<any>button)._focusMonitor.stopMonitoring(button._getHostElement());
+  // }
+
+}
